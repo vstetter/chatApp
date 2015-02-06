27a5ddd71f14335bb44b12042359c27c327532e0
Lindsay Eisberg <lindsay.eisberg@gmail.com>
2015-02-06 15:33:42 -0500

    local storage functionality for username working

--- scripts/main.js
+++ scripts/main.js
@@ -28,8 +28,7 @@ var chitChatApp = {
         // $(this).css('display', 'none');
         // $(this).siblings().addClass('active');
 
-          var strUserInfo = JSON.stringify(userInfo);
-          localStorage.setItem('userInfo', strUserInfo);
+
     });
 
 
@@ -43,26 +42,6 @@ var chitChatApp = {
     config: {
       url: 'http://tiy-fee-rest.herokuapp.com/collections/chitChat'
     },
-    //storing user data locally
-    //   profile: {
-    //     login: JSON.parse(localStorage.getItem('login')),
-    //     messages: []
-    // },
-
-
-    //
-    //   /// RETRIVE USERNAME FROM LOCAL STORAGE ////
-    //  ////////////////////////////////////////////
-    //  var profile = JSON.parse( localStorage.getItem( 'profile' ) );
-    //
-    //  /// IF USER EXISTS IN LOCAL STORAGE HIDE LOGIN FIELD ////
-    // /////////////////////////////////////////////////////////
-    //  if(localStorage.getItem('profile')) {
-    //    $('.login').html(userProfile.user.name);
-    //    $('#enterUserForm').css('display', 'none');
-    //    chatApp.renderAllUsers();
-    //  }
-
 
     renderUser: function() {
     $.ajax({
@@ -92,6 +71,8 @@ var chitChatApp = {
       success: function (data) {
         console.log(data, '', 'added!');
         chitChatApp.renderUser();
+        var strUserInfo = JSON.stringify(data);
+        localStorage.setItem('userInfo', strUserInfo);
       },
       error: function (err) {
         console.log(err);
@@ -114,67 +95,6 @@ var chitChatApp = {
     }
   });
 },
-
-  //
-  // // ---start create and render message in .chatArea ---//
-  //
-  // //initStyling:
-  // initStyling: function () {
-  //   toDoList.rendertoDoListItem();
-  // },
-  //
-  // //imitEVents:
-  //
-  //
-  // $('#createItem').on('submit', function (event) {
-  //   event.preventDefault();
-  //   var newToDoItem = {
-  //     toDoItem: $(this).find('input[name="newItem"]').val()
-  //   };
-  //   toDoList.createtoDoListItem(newToDoItem);
-  //
-  // });
-  //
-  //
-  //
-  //
-  // rendertoDoListItem: function () {
-  //   $.ajax({
-  //     url: toDoList.config.url,
-  //     type: 'GET',
-  //     success: function (toDoList) {
-  //       console.log(toDoList);
-  //       var template= _.template($('#toDoTmpl').html());
-  //       var markup = "";
-  //       toDoList.forEach(function (item, idx, arr) {
-  //         markup += template(item);
-  //       });
-  //       console.log('markup is ...', markup);
-  //       $('.ActualList').html(markup);
-  //     },
-  //     error: function (err) {
-  //       console.log(err);
-  //     }
-  //   });
-  // },
-  //
-  //
-  // createMessage: function (listItem) {
-  //   $.ajax({
-  //     url: toDoList.config.url,
-  //     data: listItem,
-  //     type: 'POST',
-  //     success: function (data) {
-  //       console.log(data);
-  //       toDoList.rendertoDoListItem();
-  //     },
-  //     error: function (err) {
-  //       console.log(err);
-  //     }
-  //   });
-  // },
-  //
-  //
 
 
 };
